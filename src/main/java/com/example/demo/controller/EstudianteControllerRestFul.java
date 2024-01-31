e4d4b6b88c57c912608af04599be7319e2b7d314
ec98 <carlospiruch98@gmail.com>
2024-01-31 18:23:24 -0500

    coneccion a DB postman y GET and POST STS

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
@@ -1,19 +1,41 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.repository.modelo.Estudiante;
+import com.example.demo.service.IEstudianteService;
 
 //una API tiene muchos servicios
 //API: por el proyecto JAVA
 
 //servicio -> controller: clase controller
 @RestController // tambien se le conoce como servicio
-
+@RequestMapping(path = "/estudiantes")
 public class EstudianteControllerRestFul {
-
+	
+	@Autowired
+	private IEstudianteService estudianteService;
+	
 	// Metodos: capacidades
-	public void guardar(Estudiante estudiante) {
-		System.out.println("guardando estudiante.");
+	@PostMapping(path = "/guardar")
+	public void guardar(@RequestBody Estudiante estudiante) {
+		this.estudianteService.guardar(estudiante);
 	}
+	
+	@GetMapping(path = "/buscar")
+	public Estudiante buscar() {
+		return this.estudianteService.buscar(3);
+	}
+	
+	@DeleteMapping(path = "/borrar")
+	public void borrar() {
+		this.estudianteService.borrar(2);
+	}
+	//http://localhost:5432:/API/v1.0/Matricula/estudiantes/buscar
 }
