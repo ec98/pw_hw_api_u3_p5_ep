3d7a0188aee79f78f891c31a3bc851d31d63b8d4
ec98 <carlospiruch98@gmail.com>
2024-01-31 19:37:27 -0500

    terminacion de verbos PGDPPA and comunication DB

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,24 +20,34 @@ import com.example.demo.service.IEstudianteService;
 @RestController // tambien se le conoce como servicio
 @RequestMapping(path = "/estudiantes")
 public class EstudianteControllerRestFul {
-	
+
 	@Autowired
 	private IEstudianteService estudianteService;
-	
+
 	// Metodos: capacidades
 	@PostMapping(path = "/guardar")
 	public void guardar(@RequestBody Estudiante estudiante) {
 		this.estudianteService.guardar(estudiante);
 	}
-	
+
 	@GetMapping(path = "/buscar")
 	public Estudiante buscar() {
 		return this.estudianteService.buscar(3);
 	}
-	
+
 	@DeleteMapping(path = "/borrar")
 	public void borrar() {
-		this.estudianteService.borrar(2);
+		this.estudianteService.borrar(3);
 	}
-	//http://localhost:5432:/API/v1.0/Matricula/estudiantes/buscar
+	// http://localhost:????/API/v1.0/Matricula/estudiantes/buscar
+
+	@PutMapping(path = "/actualizar")
+	public void actualizar(@RequestBody Estudiante estudiante) {
+		this.estudianteService.actualizar(estudiante);
+	}
+
+	@PatchMapping(path = "/actualizarParcial")
+	public void actualizarParcial(@RequestBody Estudiante estudiante) {
+		this.estudianteService.actualizarParcial(estudiante.getApellido(), estudiante.getNombre(), estudiante.getId());
+	}
 }
